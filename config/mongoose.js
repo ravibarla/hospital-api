ec65d38cfedeb544be1a324f34d885b0ccdf4642
ravi barla <ravibarla1@gmail.com>
2023-11-29 10:50:15 +0530

    fixing bugs

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,8 +1,7 @@
 import mongoose from "mongoose";
 
-mongoose.connect(`mongodb://127.0.0.1:27017/hospital-api`);
-// mongoose.connect(`mongodb+srv://ravibarla1:5rxkaVCv0ezFeayg@cluster0.zcdgmle.mongodb.net/?retryWrites=true&w=majority`)
-
+// mongoose.connect(`mongodb://127.0.0.1:27017/hospital-api`);
+mongoose.connect(`mongodb+srv://ravibarla1:5rxkaVCv0ezFeayg@cluster0.zcdgmle.mongodb.net/?retryWrites=true&w=majority`)
 export const db = mongoose.connection;
 
 db.on("error", console.error.bind(console, "error in connecting to mongodb"));
